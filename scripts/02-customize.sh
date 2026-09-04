ffbf656add98ba8f472bbaf592f01609ea338e83
GXLCLC <2193160597@qq.com>
2026-09-04 16:08:21 +0800

    Create 02-customize.sh

---
+++ scripts/02-customize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 固件自定义修改
+
+# 示例：修改默认管理 IP 为 192.168.10.1
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 示例：修改默认主机名
+# sed -i 's/OpenWrt/MyRouter/g' package/base-files/files/bin/config_generate

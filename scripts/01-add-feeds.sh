f3861a35f950707ee578b274b5d654fbec5c3541
GXLCLC <2193160597@qq.com>
2026-09-04 16:07:53 +0800

    Create 01-add-feeds.sh

---
+++ scripts/01-add-feeds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 第三方插件源添加脚本
+
+# 示例1：添加 OpenAppFilter 应用过滤
+echo "src-git oaf https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
+
+# 示例2：添加 Lean 额外 luci 插件库
+# echo "src-git coolsnowwolf_luci https://github.com/coolsnowwolf/luci.git" >> feeds.conf.default
+
+# 示例3：添加 ImmortalWrt 插件库
+# echo "src-git immortalwrt_packages https://github.com/immortalwrt/packages.git" >> feeds.conf.default
